9bb61a1cb6f06d2ca8fb482e2bb710e549109447
Nikita Kotov <nikitakotov70@gmail.com>
2022-12-29 12:10:01 +0300

    final commit
    add comen delete files

--- main.py
+++ main.py
@@ -1,38 +1,55 @@
+#create by https://github.com/nikirax
+#create with fraimworks kivy and kivymd
+#kivy: https://github.com/kivy/kivy
+#kivymd: https://github.com/kivymd/KivyMD
+ 
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.clock import Clock
 from kivy.core.window import Window
 import webbrowser
-from gtts import gTTS
 from playsound import playsound 
 from os import remove
 import pyaudio
 import wave
-from kivy.uix.screenmanager import ScreenManager, Screen
-import wavio as wv
-from kivy.uix.image import AsyncImage
 import speech_recognition as sr 
 import json
 
+#Regular application size
 #Window.size = (520, 1080)
+
+# open and read struct application
 with open("voice.kv", 'r') as file:
 	KV = file.read()
 
+#main function
 class MainApp(MDApp):
+
+	#build start menu and style application
 	def build(self):
-		#self.theme_cls.theme_style_switch_animation = True
 		self.theme_cls.theme_style = "Dark"
 		self.theme_cls.material_style = "M3"
 		self.language = True
 		return Builder.load_string(KV)
+
+		#button in settingss switch_language
 	def switch_language(self):
 		self.language = not(self.language)
+
+		#button in main menu switch dark and sun theme
 	def switch_main_theme(self):
 		self.theme_cls.theme_style = ("Dark" if self.theme_cls.theme_style == "Light" else "Light")
+
+		#button in main menu micro
 	def switch_micro_style(self):
+		#TODO: make sure that the micro does not move out when pressed
 		#self.root.ids.btn.icon = "assets/micro_red.png"
 		#playsound("C:\\Users\\User\\Desktop\\шняга\\python_test\\voice_doc\\assets\\assistent.mp3")
+
+		#asynchronous click call
 		Clock.schedule_once(self.Recognize_Stream_Audio, 0.0001)
+
+		# recognize file with voice
 	def Recognize_Stream_Audio(self, dt):
 		try:
 			filename = "assets/record.wav"
@@ -41,6 +58,8 @@ class MainApp(MDApp):
 			self.Recognize_Audio(filename)
 		except:
 			print("Error Not Text")
+
+		# record your voice
 	def Stream_Microphone(self, filename):
 		try:
 			chunk = 1024
@@ -70,26 +89,35 @@ class MainApp(MDApp):
 			wf.close()
 		except:
 			print("Error Not Text")
+
+		# recognize your voice
 	def Recognize_Audio(self, filename):
 		r = sr.Recognizer()
 		text = ""
+
 		if self.language == True:
 			loc = "en-En"
 		else:
 			loc = "ru-Ru"
+		
+		#from voice to string
 		with sr.AudioFile(filename) as source:
 			audio_data = r.record(source)
 			text = r.recognize_google(audio_data, language=loc)
+
 		remove(filename) 
 		print(text) 
 		text = text.lower()
+
+		#TODO: complete translation and recognition via json
 		if text == "болит живот" or text == "stomach aches":
 			self.Open_Tablets("analgesic")
 			print(text) 
 		elif text == "болит голова" or text == "my head":
 			print(text)
-			self.Open_Tablets("ForBelly") 
+			self.Open_Tablets("ForBelly")
 
+		# load new screen with tablets
 	def Open_Tablets(self, med):
 		if self.language == True:
 			loc = "en"
@@ -98,13 +126,18 @@ class MainApp(MDApp):
 		with open("tablets.json",'r', encoding="utf8") as file:
 			a = json.load(file)
 		self.root.current = "carousel"
+		
+		#TODO: complete carousel infinity
 		for i in range(0,3):
 			self.root.ids[f"label_{i+1}_"].text = a[med][i][f"recomendation_{loc}"]
 			self.root.ids[f"label_{i+1}"].text = a[med][i][f"name_{loc}"] 
 			self.root.ids[f"image_{i+1}"].source = a[med][i]["image"]
+		
+		#Button back in tablets screen
 	def Back_Main(self):
 		self.root.current = "main"
 
+		#Open settings animated
 	def on_chevron(self):
 		settings_card_items = [self.root.ids.volume_slider,self.root.ids.volume_value,self.root.ids.volume,self.root.ids.switch_language,self.root.ids.docs]
 		card = self.root.ids.settings_card
@@ -117,6 +150,8 @@ class MainApp(MDApp):
 			Clock.schedule_interval(self.open_settings_card, 0.0001)
 		else:
 			Clock.schedule_interval(self.close_settings_card, 0.0001)
+		
+		#Open settings animated
 	def open_settings_card(self, dt):
 		card = self.root.ids.settings_card
 		if card.size_hint[0] >= 0.7:
@@ -131,6 +166,8 @@ class MainApp(MDApp):
 		if card.pos_hint["center_y"] > 0.5:
 			card.pos_hint["center_y"] -= 0.01
 		return True
+
+		#Close settings animated
 	def close_settings_card(self, dt):
 		settings_card_items = [self.root.ids.volume_slider,self.root.ids.volume_value,self.root.ids.volume,self.root.ids.switch_language,self.root.ids.docs]
 		card = self.root.ids.settings_card
@@ -147,8 +184,11 @@ class MainApp(MDApp):
 			card.size_hint[0] -= 0.01
 		if card.size_hint[1] > 0.002:
 			card.size_hint[1] -= 0.01
+		
+		#Button in settings docs on this project
 	def open_video(self):
 		webbrowser.open('https://github.com/nikirax/Voice_Doctor', new=2)
 
-
-MainApp().run()
+#run main function
+if __name__ == '__main__':
+	MainApp().run()
